24452734811c9a99771fa1ad196c29619eb4e222
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-10-03 15:25:20 +0530

    Update Find the Town Judge.py

--- Find the Town Judge.py
+++ Find the Town Judge.py
@@ -1,4 +1,4 @@
-In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+""""In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
 
 If the town judge exists, then:
 
@@ -7,9 +7,7 @@ Everybody (except for the town judge) trusts the town judge.
 There is exactly one person that satisfies properties 1 and 2.
 You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
 
-If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1
-
-
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1"""
 
 
 
@@ -22,7 +20,7 @@ class Solution:
         a=[0]*N
         for i in range (0,N):
             a[i]=n
-            n=n+1
+            n+=1
         for i in range (0,len(trust)):
             t=trust[i][0]
             a[t-1]=-1
@@ -36,7 +34,7 @@ class Solution:
             count=0
             for i in range (0,len(trust)):
                 if(trust[i][1]==val):
-                    count=count+1
+                    count+=1
             if(count==N-1):
                 return val
             else:
